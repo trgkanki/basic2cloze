515c817bc04abaf427337375de209c2327347f32
RisingOrange <jakub.fidler@protonmail.com>
2021-08-07 18:42:44 +0200

    Make cloze shortcut work on basic notes

--- src/basic2cloze/basic2cloze.py
+++ src/basic2cloze/basic2cloze.py
@@ -1,6 +1,8 @@
+import re
+
 from aqt import gui_hooks, mw
 from aqt.addcards import AddCards
-from aqt.utils import tooltip, tr
+from aqt.utils import KeyboardModifiersPressed, tooltip, tr
 
 from .modelFinder import get_basic_note_types, get_cloze_note_types
 from .modelSelector import target_model
@@ -9,7 +11,7 @@ from .modelSelector import target_model
 def main():
     def convert_basic_to_cloze(problem, note):
         if not (
-            note._note_type in get_basic_note_types() and 
+            note._note_type in get_basic_note_types() and
             problem == tr.adding_cloze_outside_cloze_notetype()
         ):
             return problem
@@ -21,7 +23,7 @@ def main():
         old_model = mw.col.models.get(note.mid)
         front = note[old_model['flds'][0]['name']]
         back = note[old_model['flds'][1]['name']]
-        
+
         new_model = target_model(note)
         note.__init__(mw.col, new_model)
         note[new_model['flds'][0]['name']] = front
@@ -30,14 +32,43 @@ def main():
         return None
     gui_hooks.add_cards_will_add_note.append(convert_basic_to_cloze)
 
-
     def change_notetype_from_cloze_to_basic_in_addcards_dialog(addcards: AddCards):
         try:
             if addcards.notetype_chooser.selected_notetype_id in [x['id'] for x in get_cloze_note_types()]:
-                addcards.notetype_chooser.selected_notetype_id = get_basic_note_types()[0]['id']
+                addcards.notetype_chooser.selected_notetype_id = get_basic_note_types()[
+                    0]['id']
                 addcards.notetype_chooser.show()
         except Exception as e:
             print(e)
-            pass # don't cause an error when note types are missing or this code becomes outdated
-    gui_hooks.add_cards_did_init.append(change_notetype_from_cloze_to_basic_in_addcards_dialog)
+            pass  # don't cause an error when note types are missing or this code becomes outdated
+    gui_hooks.add_cards_did_init.append(
+        change_notetype_from_cloze_to_basic_in_addcards_dialog)
 
+    def add_cloze_shortcut_that_works_on_basic_notes(shortcuts, editor):
+
+        # code adapted from original onCloze and _onCloze
+        def myOnCloze(self):
+            self.call_after_note_saved(
+                lambda: _myOnCloze(editor), keepFocus=True)
+
+        def _myOnCloze(self):
+            if self.note.model() not in get_basic_note_types():
+                return
+            # find the highest existing cloze
+            highest = 0
+            for name, val in list(self.note.items()):
+                m = re.findall(r"\{\{c(\d+)::", val)
+                if m:
+                    highest = max(highest, sorted([int(x) for x in m])[-1])
+            # reuse last?
+            if not KeyboardModifiersPressed().alt:
+                highest += 1
+            # must start at 1
+            highest = max(1, highest)
+            self.web.eval("wrap('{{c%d::', '}}');" % highest)
+
+        shortcuts.append(("Ctrl+Shift+C", lambda: myOnCloze(editor)))
+        shortcuts.append(("Ctrl+Shift+Alt+C", lambda: myOnCloze(editor)))
+
+    gui_hooks.editor_did_init_shortcuts.append(
+        add_cloze_shortcut_that_works_on_basic_notes)
